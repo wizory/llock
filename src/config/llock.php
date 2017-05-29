16d558fe43f71c70463cf557a1b7331d84ca356b
Josh Guice <jguice@gmail.com>
2017-05-29 14:21:04 -0700

    feat(retry): reduce retry interval

--- src/config/llock.php
+++ src/config/llock.php
@@ -2,7 +2,7 @@
 
 return [
     # amount of time to wait between retries for obtaining a lock (when --wait is used)
-    'wait-retry' => 15,  # seconds
+    'wait-retry' => 10,  # seconds
 
     # timeout when waiting to obtain a lock (returns a failure after this time expires)
     'timeout' => 600,  # seconds (5 minutes)
